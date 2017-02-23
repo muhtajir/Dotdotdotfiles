5e9eb90ce09c917c4d7779937716113e24278e18
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-02-23 14:39:37 +0100

    Use exit codes in shutdown blocklet

--- .config/i3blocks/blocklets/shutdown.sh
+++ .config/i3blocks/blocklets/shutdown.sh
@@ -1,10 +1,22 @@
 echo ''
 if [[ $BLOCK_BUTTON = 1 ]]; then
-    yad --button=' _Herunterfahren':'systemctl poweroff' \
-        --button=' _Neustarten':'systemctl reboot'\
-        --button=' _Ruhezustand':'systemctl suspend'\
+    yad --button=' _Herunterfahren':21 \
+        --button=' _Neustarten':22 \
+        --button=' _Bereitschaft':23 \
         --button=gtk-close \
         --title=Herunterfahren \
         --text='System herunterfahren?\n' \
         --text-align=center
+
+    case $? in
+        21)
+            systemctl poweroff
+            ;;
+        22)
+            systemctl reboot
+            ;;
+        23)
+            systemctl suspend
+            ;;
+    esac
 fi

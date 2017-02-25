1896a069bbadfef021a3405dd4dcd3b8c289a0a1
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-02-25 13:29:15 +0100

    Let network blocklet handle both wifi and wired

--- .config/i3blocks/blocklets/network.sh
+++ .config/i3blocks/blocklets/network.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 
+WIRED_INSTANCE=${BLOCK_INSTANCE/;*/}
+WIFI_INSTANCE=${BLOCK_INSTANCE/*;/}
+
 # wired connection
-WIRED_INSTANCE=${BLOCK_INSTANCE%%;*}
-WIFI_INSTANCE=${BLOCK_INSTANCE##*;}
-STATE=$(cat /sys/class/net/${WIRED_INSTANCE}/operstate)
-if [[ $STATE == "up" ]]; then
-    if [[ $(ip route | grep ${WIRED_INSTANCE}) ]]; then
-        wired='<span foreground="#8ca06b"></span>'
-    else
-        wired='<span foreground="#fdcc59"></span>'
+if [[ -n $WIRED_INSTANCE ]]; then
+    STATE=$(cat /sys/class/net/${WIRED_INSTANCE}/operstate)
+
+    if [[ $STATE == "up" ]]; then
+        if [[ $(ip route | grep ${WIRED_INSTANCE}) ]]; then
+            wired='<span foreground="#8ca06b"></span>'
+        else
+            wired='<span foreground="#fdcc59"></span>'
+        fi
     fi
 fi
 
-echo -n $wired
-# stop here if there's only one interface in block_instance
-[[ $WIRED_INSTANCE = $WIFI_INSTANCE ]] && exit
-
 # wifi connection
-WIFI_INTERFACE="${WIFI_INSTANCE:-wlan0}"
-QUALITY=$(grep $WIFI_INTERFACE /proc/net/wireless | awk '{ print int($3 * 100 / 70) }')
-WIFI_SSID=$(iwgetid -r)
+if [[ -n $WIFI_INSTANCE ]]; then
+    if [[ ! $(cat /sys/class/net/${WIFI_INSTANCE}/operstate) = 'up' ]];then
+    wifi=''
+    else
+        QUALITY=$(grep $WIFI_INTERFACE /proc/net/wireless | awk '{ print int($3 * 100 / 70) }')
+        WIFI_SSID=$(iwgetid -r)
 
-# stop here if wifi interface isn't up
-[[ ! $(cat /sys/class/net/${WIFI_INSTANCE}/operstate) = 'up' ]] && exit
-
-# color
-if [[ $QUALITY -ge 80 ]]; then
-    mark_front='<span foreground="#8ca06b">'
-elif [[ $QUALITY -lt 80 ]]; then
-    mark_front='<span foreground="#fdcc59">'
-elif [[ $QUALITY -lt 40 ]]; then
-    mark_front='<span foreground="#dd464c">'
+        if [[ $QUALITY -ge 80 ]]; then
+            mark_front='<span foreground="#8ca06b">'
+        elif [[ $QUALITY -ge 60 ]]; then
+            mark_front='<span foreground="#fdcc59">'
+        elif [[ $QUALITY -ge 40 ]]; then
+            mark_front='<span foreground="#fd8b19">'
+        else
+            mark_front='<span foreground="#dd464c">'
+        fi
+        wifi="${mark_front}</span>"
+    fi
 fi
-wifi="${mark_front}</span>"
 
-[[ $wired ]] && echo ' '
-echo "$wifi ${WIFI_SSID}" # full text
-echo "$wifi" # short text
+# full text
+echo "${wired}${wifi:+${wired:+ }$wifi${WIFI_SSID:+ <span size='small'>${WIFI_SSID}</span>}}"
+# short text
+echo "${wired}${wifi:+${wired:+ }$wifi}"

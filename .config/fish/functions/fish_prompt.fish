50dc833274531f435ffdbdfe4aefdee804003c39
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-06-01 02:21:27 +0200

    Improve fish prompt structure

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,6 +1,14 @@
 function fish_prompt --description 'Write out the prompt'
-    set_color $color04; and printf '░▒▓'
-    set_color -b $color04 ; and set_color $color18; and printf ' %s ' (prompt_pwd)
-    set_color -b normal; set_color $color04; and printf ' ' 
-    set_color -b normal normal
+    chcol $color04; and printf '░▒▓'
+    chcol $color18 $color04 ; and printf ' %s ' (prompt_pwd)
+    chcol $color04 normal; and printf ' ' 
+    chcol normal normal
+end
+
+function chcol -a fg bg
+    if test -n "$bg"
+        set_color -b $bg $fg
+    else
+        set_color $fg
+    end
 end

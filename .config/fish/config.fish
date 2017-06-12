dcf99800d1d166c042e269ddfa0b4a4f5f6b32cd
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-06-12 15:08:59 +0200

    Update .gitignore

---
+++ .config/fish/config.fish
@@ -0,0 +1,24 @@
+# set our abbreviations if there are none
+# (updates have to be done manually)
+if not count $fish_user_abbreviations > /dev/null
+    fish_set_user_abbreviations
+end
+
+# set global variables for this machine if they have never been set before
+# (updates also have to be done manually
+if not set -q fish_default_variables_set
+    fish_set_universal_variables
+end
+
+# set color scheme if it hasn't bet set yet
+# (blablabla updates)
+if not set -q color01
+    fish_set_base16_colors
+end
+
+# always set aliases as these are only set by shell instance
+# (so prefer abbreviations)
+fish_set_user_aliases
+
+# initialize event handlers
+fish_load_event_handlers

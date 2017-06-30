f080327bdaf0261bdd817457cc9d68605600188f
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-06-30 19:10:42 +0200

    Simplify shell colors

--- .config/fish/functions/__fish_create_first_prompt_seg.fish
+++ .config/fish/functions/__fish_create_first_prompt_seg.fish
@@ -5,16 +5,16 @@ function __fish_create_first_prompt_seg --description 'Put together first prompt
     # now choose content for the first prompt segment
     # highest ranking output is bind mode
     if test $fish_bind_mode != insert
-        __fish_draw_first_prompt_seg $color16 " Φ "
+        __fish_draw_first_prompt_seg yellow " Φ "
     # second is last exit code
     else if test $return_code -gt 0
-        __fish_draw_first_prompt_seg $color01 " $return_code "
+        __fish_draw_first_prompt_seg red " $return_code "
     # least important is active jobs
     else if jobs > /dev/null ^ /dev/null
         set -l fish_last_job (jobs -lc | tail -n 1)
-        __fish_draw_first_prompt_seg $color06 " $fish_last_job "
+        __fish_draw_first_prompt_seg cyan " $fish_last_job "
     # and after that it's just a percent sign
     else
-        __fish_draw_first_prompt_seg $color04 ' %% '
+        __fish_draw_first_prompt_seg blue ' %% '
     end
 end
--- .config/fish/functions/__fish_draw_git_prompt_seg.fish
+++ .config/fish/functions/__fish_draw_git_prompt_seg.fish
@@ -1,8 +1,8 @@
 function __fish_draw_git_prompt_seg -a content --description 'Put together a git segment that changes color if the repo is not in a clean state'
     set -l git_state (command git status --porcelain ^ /dev/null)
     if test -z "$git_state"
-        __fish_draw_second_prompt_seg $color21 ""$content
+        __fish_draw_second_prompt_seg brwhite ""$content
     else
-        __fish_draw_second_prompt_seg $color13 ""$content
+        __fish_draw_second_prompt_seg brmagenta ""$content
     end
 end

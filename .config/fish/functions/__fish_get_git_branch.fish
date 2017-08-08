4552bd6e388fcb76eb9391c1260b30137d5e1649
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-08-08 13:45:08 +0200

    Fix git prompt for branchless repos

--- .config/fish/functions/__fish_get_git_branch.fish
+++ .config/fish/functions/__fish_get_git_branch.fish
@@ -1,3 +1,5 @@
 function __fish_get_git_branch
-    echo (command git rev-parse --abbrev-ref HEAD)
+    set -l git_branch (command git rev-parse --abbrev-ref HEAD ^ /dev/null)
+    test "$git_branch" = "HEAD"; and set git_branch "…"
+    echo $git_branch
 end

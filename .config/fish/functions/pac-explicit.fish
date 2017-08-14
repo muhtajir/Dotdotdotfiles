33ab0b4e1a43d46825494f4c0e4add172f330ebc
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-08-14 15:29:58 +0200

    Plus one char in pac-explicit.fish

--- .config/fish/functions/pac-explicit.fish
+++ .config/fish/functions/pac-explicit.fish
@@ -1,6 +1,5 @@
-# Defined in /tmp/fish.pGK51w/pac-explicit.fish @ line 2
 function pac-explicit
-	set -l pacs (pacman -Qeq)
+	set -l pacs (pacman -Qetq)
     for pac in $pacs
         set -l group (pacman -Qi $pac | grep '^Gruppen' | string replace -r '^[^:]+:\s+(.+)$' '$1')
         string match -rq '^base' $group

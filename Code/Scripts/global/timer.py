bb17a9a6c9f515086735e34682416d94e6f104e8
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-08-28 14:21:35 +0200

    Restructure shared scripts

---
+++ Code/Scripts/global/timer.py
@@ -0,0 +1,12 @@
+import sys
+import time
+timer_time = 0
+
+while True:
+    mins = timer_time / 60
+    secs = timer_time % 60
+    output = '{:02.0f}:{:02}'.format(mins, secs)
+    print(output, sep=' ', end='\r', flush=True)
+    sys.stdout.flush()
+    time.sleep(1)
+    timer_time += 1

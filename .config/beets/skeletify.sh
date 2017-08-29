da7acbc904991fe56a539c8cdef4a316b7bcb9f4
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-08-29 22:24:26 +0200

    More theming

--- .config/beets/skeletify.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed -r 's/(\s*google.+?key.*:).+/#\1 restricted/gi' config.yaml > skel.tmp
-mv skel.tmp config.yaml.skel
--- Code/Scripts/global/merge_configs.sh
+++ Code/Scripts/global/merge_configs.sh
@@ -3,5 +3,7 @@
 folder_list=( "$HOME/.config/i3"  "$HOME/.config/sway"  "$HOME/.config/i3blocks")
 for folder in ${folder_list[@]}; do
     cd $folder
-    echo -e "$(cat config.skel)\n\n# CUSTOM CONFIG\n\n$(cat custom)" > config
+    if [[ ! -L config ]]; then
+        echo -e "$(cat config.skel)\n\n# CUSTOM CONFIG\n\n$(cat custom)" > config
+    fi
 done

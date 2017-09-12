9c12945ade971f2f2899cdd4534f9842a0c9f8f6
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-09-12 16:12:48 +0200

    Improve pac-explicit

--- .config/fish/functions/pac-explicit.fish
+++ .config/fish/functions/pac-explicit.fish
@@ -1,10 +1,12 @@
 function pac-explicit
 	set -l pacs (pacman -Qetq)
+    set -l explicits
     for pac in $pacs
         set -l group (pacman -Qi $pac | grep '^Gruppen' | string replace -r '^[^:]+:\s+(.+)$' '$1')
         string match -rq '^base' $group
         if test $status -ne 0
-            echo $pac
+            set explicits $explicits $pac
         end
     end
+    string join \n $explicits
 end

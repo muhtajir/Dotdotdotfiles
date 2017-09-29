d841f676afafcb52873057232e44ea0e32570f66
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-09-29 20:46:50 +0200

    Improve py.test function

--- .config/fish/functions/py.test.fish
+++ .config/fish/functions/py.test.fish
@@ -1,4 +1,16 @@
 function py.test
+    set -l curdir (pwd)
+
+    if test -d tests
+        true
+    else if test -d ../tests
+        cd ..
+    else
+        echo 'Directory "tests" not found.'
+        return
+    end
+
     set -lx PYTHONPATH (pwd)
     pytest $argv
+    cd $curdir
 end

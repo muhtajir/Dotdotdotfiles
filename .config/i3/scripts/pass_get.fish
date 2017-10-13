3f759f4549ccc7f1a03072752f5e019575eb7956
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-10-13 23:14:46 +0200

    Update pass_get

--- .config/i3/scripts/pass_get.fish
+++ .config/i3/scripts/pass_get.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/env fish
 
 test (count $argv) -eq 1; and test $argv[1] = '-l'; and set -l login_mode
+test -z "$PASS_SUBNAME_SPLIT_CHAR"; and set -l PASS_SUBNAME_SPLIT_CHAR '%'
+
 # get name of currently active window
 set -l active_line (string match -ri '^_NET_ACTIVE_WINDOW\(WINDOW\).+' (xprop -root))
 set -l active_id (string replace -r '^[^#]+#\s(\S+)' '$1' $active_line)
@@ -20,6 +22,24 @@ for password in $passes
         set candidates $candidates $password
     end
 end
+
+# if there are no matches try secondary names
+if test -z "$candidates"
+    for password in $passes
+        set -l barepass (command basename $password)
+        set -l subs (string split "$PASS_SUBNAME_SPLIT_CHAR" "$barepass")
+        test $status -eq 0; and for sub in $subs
+            echo $sub
+            echo $name
+            string match -qi "*$sub*" $name
+            if test $status -eq 0
+                set candidates $password
+            end
+        end
+    end
+end
+
+echo $candidates
 
 # send candidate to pass; use dmenu to select one if there are too many
 set -l cand_num (count $candidates)

ef6f5cb24c629173f24dffce1b6b11b7d7fcf263
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-10-18 00:37:25 +0200

    Set fzf colorscheme in fish

--- .config/fish/functions/fish_set_universal_variables.fish
+++ .config/fish/functions/fish_set_universal_variables.fish
@@ -4,6 +4,5 @@ function fish_set_universal_variables
     set -U fish_escape_delay_ms 10
     set -U fish_function_path_local $HOME/.config/fish/functions
     set -U FZF_TMUX_HEIGHT 60%
-    set -U FZF_DEFAULT_OPTS "--bind=alt-j:down,alt-k:up --reverse"
     set -U FZF_ALT_C_COMMAND "find \( -wholename '*.config/*' -or \( -wholename '*.local/share/*' -and -not -wholename '*.local/share/Steam/*' \) \) -or -not -path '*/\.*' ^ /dev/null"
 end
--- .config/fish/functions/fish_update_user_settings.fish
+++ .config/fish/functions/fish_update_user_settings.fish
@@ -51,7 +51,5 @@ function fish_update_user_settings
 
     fish_set_user_abbreviations
 
-    # this last one is redundant for now, as all aliases are set at shell
-    # startup
-    # fish_set_user_aliases
+    fish_set_fzf_colorscheme
 end

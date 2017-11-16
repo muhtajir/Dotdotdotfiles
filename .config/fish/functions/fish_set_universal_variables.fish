de5ff41a225bbbfb54aacdfd5d4aa21e44152110
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-11-16 23:37:36 +0100

    Sexier merge_config

--- .config/fish/functions/fish_set_universal_variables.fish
+++ .config/fish/functions/fish_set_universal_variables.fish
@@ -3,6 +3,7 @@ function fish_set_universal_variables
     set -U fish_default_variables_set
     set -U fish_escape_delay_ms 10
     set -U fish_function_path_local $HOME/.config/fish/functions
+    set -Ux LS_COLORS 'ow=34'
     set -U FZF_TMUX_HEIGHT 60%
     set -U FZF_ALT_C_COMMAND "command find -P \$dir -mindepth 1 \( -wholename '*.config/*' -or \( -wholename '*.local/share/*' -and -not -wholename '*.local/share/Steam/*' \) \) -or -not -path '*/\.*' 2> /dev/null"
     set -U FZF_CTRL_T_COMMAND "command find -P \$dir -mindepth 1 \( -wholename '*.config/*' \) -or -not -path '*/\.*' 2> /dev/null"

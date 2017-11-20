df203e1b040f533d149cd8db95445616e501f578
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-11-20 16:13:09 +0100

    Handle ssh-agent errors

--- .config/fish/functions/fish_initialize_ssh_agent.fish
+++ .config/fish/functions/fish_initialize_ssh_agent.fish
@@ -1,4 +1,9 @@
 function fish_initialize_ssh_agent
+    # if ssh isn't installed/set-up, do nothing
+    if not which ssh 1>&- 2>&-; or not test -e $HOME/.ssh
+        return
+    end
+
     if test -z "$SSH_ENV"
         set -xg SSH_ENV $HOME/.ssh/environment
     end

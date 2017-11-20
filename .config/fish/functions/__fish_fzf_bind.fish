6979e986c6aace26ee2a1d13038485efd13b2b07
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-11-20 17:22:24 +0100

    Implement custom man function

--- .config/fish/functions/__fish_fzf_bind.fish
+++
@@ -1,6 +0,0 @@
-function __fish_fzf_bind --description 'Wrapper for fzf as a key bind.'
-    set -l fzf_result (fzf --bind alt-j:down,alt-k:up --reverse)
-    commandline -i "$fzf_result"
-    commandline -f repaint
-end
-

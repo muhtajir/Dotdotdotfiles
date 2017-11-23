d319048092d1bb8e7789b5e37f3e9a8752916e70
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-11-23 22:17:12 +0100

    Adapt pass_get to custom pass directory

--- .config/i3/scripts/pass_get.fish
+++ .config/i3/scripts/pass_get.fish
@@ -1,7 +1,8 @@
 #!/usr/bin/env fish
 
 test (count $argv) -eq 1; and test $argv[1] = '-l'; and set -l login_mode
-test -z "$PASS_SUBNAME_SPLIT_CHAR"; and set -l PASS_SUBNAME_SPLIT_CHAR '%'
+test -z $PASS_SUBNAME_SPLIT_CHAR; and set -l PASS_SUBNAME_SPLIT_CHAR '%'
+test -z $PASSWORD_STORE_DIR; and set -l PASSWORD_STORE_DIR $HOME/.password-store
 
 # get name of currently active window
 set -l active_line (string match -ri '^_NET_ACTIVE_WINDOW\(WINDOW\).+' (xprop -root))
@@ -11,7 +12,7 @@ set -l name (string match -r '".+?"$' $name_line)
 set name (string replace -a ' ' '_' $name)
 
 # get list of all pass files
-set -l passes (string replace -r '^.+?\.password-store/(.+?)\.gpg$' '$1' $HOME/.password-store/**/*)
+set -l passes (string replace -r '^.+?\.password-store/(.+?)\.gpg$' '$1' $PASSWORD_STORE_DIR/**/*)
 
 # loop through files and find one that matches
 set -l candidates

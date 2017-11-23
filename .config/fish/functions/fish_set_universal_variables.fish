66afa35173d4f49b3d972be4e7255ed6dbe009b4
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-11-23 18:11:35 +0100

    Move pass directory

--- .config/fish/functions/fish_set_universal_variables.fish
+++ .config/fish/functions/fish_set_universal_variables.fish
@@ -3,9 +3,14 @@ function fish_set_universal_variables
     set -U fish_default_variables_set
     set -U fish_escape_delay_ms 10
     set -U fish_function_path_local $HOME/.config/fish/functions
+
+    # environment variables used by other programs
+    # FZF
     set -U FZF_TMUX_HEIGHT 60%
     set -U FZF_ALT_C_COMMAND "command find -P \$dir -mindepth 1 \( -wholename '*.config/*' -or \( -wholename '*.local/share/*' -and -not -wholename '*.local/share/Steam/*' \) \) -or -not -path '*/\.*' 2> /dev/null"
     set -U FZF_CTRL_T_COMMAND "command find -P \$dir -mindepth 1 \( -wholename '*.config/*' \) -or -not -path '*/\.*' 2> /dev/null"
+    # pass
+    set -Ux PASSWORD_STORE_DIR $HOME/.local/share/.password-store
 
     # shell and pager color settings
     set -Ux LS_COLORS 'ow=34'

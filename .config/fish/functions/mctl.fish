e4075c4a69978fc9f049f509a3b0f977418f008c
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-12-17 11:59:37 +0100

    Extend i3 mpd integration

--- .config/fish/functions/mctl.fish
+++ .config/fish/functions/mctl.fish
@@ -1,4 +1,4 @@
-function musicctl
+function mctl
     # start mpd if it isn't running
     if not systemctl --user is-active mpd.service >/dev/null
         systemctl --user start mpd.service
--- .config/fish/functions/plctl.fish
+++ .config/fish/functions/plctl.fish
@@ -1,4 +1,4 @@
-function playlistctl
+function plctl
     # start mpd if it isn't running
     if not systemctl --user is-active mpd.service >/dev/null
         systemctl --user start mpd.service

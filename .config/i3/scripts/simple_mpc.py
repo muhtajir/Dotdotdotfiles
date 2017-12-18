02478f2d71cae947f1f3b2a3c1613ff0d1bf1eb5
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-12-18 23:55:21 +0100

    Add mpd random switch to i3

--- .config/i3/scripts/simple_mpc.py
+++ .config/i3/scripts/simple_mpc.py
@@ -15,6 +15,15 @@ def start_mpd():
         subprocess.run(['mpdscribble'])
 
 
+def random_toggle(mpd_client):
+    if mpd_client.status()['random'] == '0':
+        mpd_client.random(1)
+        subprocess.run(['notify-send', 'Random mode turned on.'])
+    elif mpd_client.status()['random'] == '1':
+        mpd_client.random(0)
+        subprocess.run(['notify-send', 'Random mode turned off.'])
+
+
 # initialize mpd connection
 mpd_client = MPDClient()
 try:
@@ -35,7 +44,4 @@ elif command == 'next':
 elif command == 'previous':
     mpd_client.previous()
 elif command == 'random_toggle':
-    if mpd_client.status()['random'] == 0:
-        mpd_client.random(1)
-    elif mpd_client.status()['random'] == 1:
-        mpd_client.random(0)
+    random_toggle(mpd_client)

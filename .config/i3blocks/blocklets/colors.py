6d03c7bac0714920e073eaf99867325eb4578f2b
Pu Anlai <inspectormustache@users.noreply.github.com>
2018-03-08 20:18:14 +0100

    Remove blocklet color scripts

--- .config/i3blocks/blocklets/colors.py
+++
@@ -1,5 +0,0 @@
-import imp
-import os
-WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
-imp.load_source('colors', '{}/colors'.format(WORKING_DIR))
-from colors import * # i think this is a case where it's okay to do this

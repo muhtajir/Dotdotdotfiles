28fd58425b01772b26a6e025020a51789b935bf4
Pu Anlai <inspectormustache@users.noreply.github.com>
2018-03-26 14:57:22 +0200

    Only resync mail on mailbox change

--- .config/fish/functions/mymail.fish
+++ .config/fish/functions/mymail.fish
@@ -1,6 +1,14 @@
 function mymail
+    set -l current_dir (pwd)
+    set -l mutt_dir "$HOME/Downloads/Mutt"
+
+    mkdir -p $mutt_dir
+    cd $mutt_dir
+
 	mbsync -a
     notmuch new 2> /dev/null
     neomutt
 	and mbsync -a
+
+    cd $current_dir
 end

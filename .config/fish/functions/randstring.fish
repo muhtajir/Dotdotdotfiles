4a94564bbe17465cf494436fa1ed72100285b5a2
Pu Anlai <inspectormustache@users.noreply.github.com>
2018-05-20 20:10:42 +0200

    Allow xdotool flag for randstring

--- .config/fish/functions/randstring.fish
+++ .config/fish/functions/randstring.fish
@@ -2,11 +2,15 @@ function randstring --description 'Generate a random string'
     # argparsing
     # function to validate input is int
 
-    argparse -n randstring 'l/length=!_validate_int' 'c/charset=' -- $argv; or return 1
+    argparse -n randstring 'l/length=!_validate_int' 'c/charset=' 'x' -- $argv; or return 1
     test -n "$_flag_l"; and set -l length $_flag_l; or set -l length 25
     test -n "$_flag_c"; and set -l charset $_flag_c; or set -l charset '[:graph:]'
 
     set -lx LC_ALL C
     tr -dc "$charset" < /dev/urandom | read -l -n $length randstring
-    echo $randstring
+    if [ -n "$_flag_x" ]
+        sleep 2; xdotool type $randstring
+    else
+        echo $randstring
+    end
 end

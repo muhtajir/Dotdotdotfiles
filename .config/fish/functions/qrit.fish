c18766d952008f354295ee4a80eff03c2279bcdb
Pu Anlai <inspectormustache@users.noreply.github.com>
2018-06-11 01:29:39 +0200

    Fix qrit function

--- .config/fish/functions/qrit.fish
+++ .config/fish/functions/qrit.fish
@@ -1,10 +1,16 @@
 function qrit -d "Create a QR Code from the provided argument"
-   [ -z "$argv" ]; and return 1
-
-   set -l str_out $argv[1]
-    for str in $argv[2..-1]
-           set str_out $str_out" $str"
+    set -l str_out
+    switch (count $argv)
+        case 0
+            return 0
+        case 1
+            set str_out $argv
+        case '*'
+            set str_out $argv[1]
+            for str in $argv[2..-1]
+                set str_out $str_out" $str"
+            end
     end
 
-   echo -n "$str_out" | qrencode --size 10 -o - | feh -. -
+    echo -n "$str_out" | qrencode --size 10 -o - | feh -. -
 end

1d63cf14c2119bc947f802e223a6bad8d7e306c0
Pu Anlai <inspectormustache@users.noreply.github.com>
2018-10-24 16:08:48 +0200

    Add some everyday keybindings

---
+++ .config/fish/functions/adbfsmount.fish
@@ -0,0 +1,11 @@
+function adbfsmount --description "Provide a adbfs mountpoint as $adbfsmnt"
+    if [ -d "$adbfsmnt" ]
+        set_color -o; echo "Unmounting \$adbfsmnt ($adbfsmnt)..."; set_color normal
+        fusermount -u "$adbfsmnt"
+        set -eU adbfsmnt
+    else
+        set -U adbfsmnt (mktemp -d --suffix "-adbbfs")
+        set_color -o; echo "Mounting under $adbfsmnt (\$adbfsmnt)..."; set_color normal
+        adbfs "$adbfsmnt"
+    end
+end

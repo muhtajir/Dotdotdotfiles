04706f0697e67cb1130bf9dd1a34bd13f6677b18
Pu Anlai <inspectormustache@users.noreply.github.com>
2018-12-20 15:20:26 +0100

    Handle error codes in adbfsmount.fish

--- .config/fish/functions/adbfsmount.fish
+++ .config/fish/functions/adbfsmount.fish
@@ -2,10 +2,11 @@ function adbfsmount --description "Provide a adbfs mountpoint as $adbfsmnt"
     if [ -d "$adbfsmnt" ]
         set_color -o; echo "Unmounting \$adbfsmnt ($adbfsmnt)..."; set_color normal
         fusermount -u "$adbfsmnt"
-        set -eU adbfsmnt
+        and set -eU adbfsmnt
     else
         set -U adbfsmnt (mktemp -d --suffix "-adbbfs")
         set_color -o; echo "Mounting under $adbfsmnt (\$adbfsmnt)..."; set_color normal
         adbfs "$adbfsmnt"
+        or set -eU adbfsmnt
     end
 end

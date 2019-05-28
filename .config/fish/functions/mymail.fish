13d379580969539c78982d9e290530f2e5c917ab
Pu Anlai <inspectormustache@users.noreply.github.com>
2019-05-28 23:04:37 +1000

    Let mymail wait for mbsync to finish

--- .config/fish/functions/mymail.fish
+++ .config/fish/functions/mymail.fish
@@ -1,4 +1,10 @@
 function mymail
+    while pgrep mbsync >/dev/null
+        echo -n 'Waiting for mbsync to finish.'
+        sleep 2
+        echo -ne '\r\033[2K'
+    end
+
     set -l current_dir (pwd)
     set -l mutt_dir "$HOME/Downloads/Mutt"
 

a0c6943538daa9e53bb1e05b32bd05cc76d0005a
Pu Anlai <inspectormustache@users.noreply.github.com>
2019-06-05 22:50:55 +1000

    Adapt pybuild for fish 3

--- .config/fish/functions/pybuild.fish
+++ .config/fish/functions/pybuild.fish
@@ -2,10 +2,10 @@ function pybuild
     [ -e setup.py ]; or return 1
     python setup.py sdist bdist_wheel
     pass show -c random/package-signing
-    set -l version (string replace -rf '^\s+version=.([\d.]+).+' '$1' < setup.py) 
+    set -l pkg_version (string replace -rf '^\s+version=.([\d.]+).+' '$1' < setup.py) 
     cd dist
-    gpg --detach-sign --local-user 7F6D8FF4 -a *$version*
+    gpg --detach-sign --local-user 7F6D8FF4 -a *$pkg_version*
     pass show -c random/python_package_index\%twine
-    twine upload *$version*
+    twine upload *$pkg_version*
     cd ..
 end

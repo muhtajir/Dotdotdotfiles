c4cd6a2d40fdab9d3f482dd4f0caaf237441cb6e
Pu Anlai <inspectormustache@users.noreply.github.com>
2019-06-09 14:01:49 +1000

    Upgrade aurmake for the new millenium

--- .config/fish/functions/aurmake.fish
+++ .config/fish/functions/aurmake.fish
@@ -1,39 +1,41 @@
 function aurmake -w cower -d 'Build specified AUR package'
-    set -l pkgs (string match -r '(?<!\x2d)\b\S+' -- $argv)
-    set -l args (string match -r '(?<!\S)\x2d(\x2d)?\S+' -- $argv)
-    set -l all_deps
+    set pkgs (string match -r '(?<!\x2d)\b\S+' -- $argv)
+    set args (string match -r '(?<!\S)\x2d(\x2d)?\S+' -- $argv)
 
-    true
+    # building packages
     for pkg in $pkgs
-        set -l all_pkgs (auracle buildorder $pkg | string match -r '(?<=^BUILD )\S+')
-
-        # if the package itself is not included in the buildorder list include it here
-        if [ "$all_pkgs[-1]" != $pkg ]
-            set all_pkgs $all_pkgs $pkg
-        end
-
-        if [ (count $all_pkgs) -gt 1 ]
-            set_color -o
-            echo "Building dependencies..."
-            for dep in $all_pkgs[1..-2]
-                __aurmake_single --asdeps $dep
-                or return 1
-                set all_deps $all_deps $dep
+        # parsing `auracle buildorder`
+        for dep in (auracle buildorder $pkg | string match 'AUR*')
+            set dep (string split ' ' $dep)
+            if not contains $dep[-1] $aur_deps
+                set -a aur_deps $dep[-1]
             end
         end
 
+        # building AUR dependencies
+        for dep in $aur_deps
+            set_color -o && echo "Building dependency $dep for package $pkg..." && set_color normal
+            __aurmake_single --asdeps $dep
+            or return 1
+        end
+
+        # building the target package
         __aurmake_single $args $pkg
         or return 1
 
     end
 
-    set_color -o
-    set -l del_deps
-    for dep in (pacman -Qdtq)
-         contains $dep $all_deps; and set del_deps $del_deps $dep
+    # finding no longer needed build dependencies...
+    set orphans (pacman -Qdtq)
+    for dep in $aur_deps
+        if contains $dep $orphans
+            set -a del_deps $dep
+        end
     end
+
+    # ...and removing them
     if [ -n "$del_deps" ]
-        echo "Removing dependencies installed from AUR..."
+        set_color -o && echo "Removing build dependencies installed from AUR..." && set_color normal
         sudo pacman -Rsc $del_deps
     end
 end

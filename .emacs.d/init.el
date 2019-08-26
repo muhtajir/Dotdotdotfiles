21aab86ae750e480321c8ac6b20fb5054d05c37f
Pu Anlai <inspectormustache@users.noreply.github.com>
2019-08-26 20:45:02 +1000

    Delete index.lock from within emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -112,7 +112,14 @@ Replace buffer/window if in helpful-mode, lazy-open otherwise."
 (use-package magit
   :commands magit-status
   :hook ((magit-mode . my/source-ssh-env)
-         (with-editor-mode . evil-insert-state)))
+         (with-editor-mode . evil-insert-state))
+  :config
+  (defun my/force-git-access ()
+    (interactive)
+      (let ((index-file (concat
+                         (projectile-project-root) (file-name-as-directory ".git") "index.lock")))
+        (when (yes-or-no-p (concat "Really delete " index-file "?"))
+          (delete-file index-file)))))
 
 (use-package pcre2el
   :defer t)

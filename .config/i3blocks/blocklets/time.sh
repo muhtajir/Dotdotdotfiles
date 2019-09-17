f9f8bee9b0e85f01cb5a5cb7e577f93ab2747587
Pu Anlai <inspectormustache@users.noreply.github.com>
2019-09-17 16:23:41 +1000

    Make time blocklet switch between date and time

--- .config/i3blocks/blocklets/time.sh
+++ .config/i3blocks/blocklets/time.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
-echo $(date +%H:%M)
+function print_time_info {
+    case ${_BLOCK_STATE} in
+        time)
+            current_time=$(date +%H:%M )
+            ;;
+        date)
+            current_time=$(date +%a,\ %e.%m.%Y)
+            ;;
+    esac
+    echo "{\"full_text\" : \"$current_time\", \"_BLOCK_STATE\" : \"$_BLOCK_STATE\"}"
+}
 
-if [[ ${BLOCK_BUTTON} -eq 1 ]]; then
-    notify-send --icon=none "$(date +%A,\ %d.%m.%Y)" "\n$(cal)"
-fi
+function get_new_state {
+    case ${_BLOCK_STATE} in
+        time)
+            echo date
+            ;;
+        date)
+            echo time
+            ;;
+    esac
+}
+
+case ${BLOCK_BUTTON} in 
+    3)
+        notify-send --icon=none "$(date +%A,\ %d.%m.%Y)" "\n$(cal)"
+        print_time_info
+    ;;
+    1)
+        _BLOCK_STATE=$(get_new_state)
+        print_time_info
+    ;;
+    *)
+        print_time_info
+    ;;
+esac

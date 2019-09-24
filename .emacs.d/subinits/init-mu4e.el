4d8031ef68e6b5e09b9787e53a58bb719e45380e
Pu Anlai <inspectormustache@users.noreply.github.com>
2019-09-25 01:02:36 +1000

    Extend mu4e config

--- .emacs.d/subinits/init-mu4e.el
+++ .emacs.d/subinits/init-mu4e.el
@@ -19,6 +19,26 @@
         mu4e-drafts-folder        "/Drafts"
         mu4e-trash-folder         "/Trash"
         mu4e-refile-folder        "/Archiv"
-        mu4e-get-mail-command     "mbsync -V posteo"))
+        mu4e-get-mail-command     "mbsync -V posteo"
+        mu4e-user-mail-address-list '("{{MAIL_ADDRESS}}"))
+  ;; headers view
+  (setq mu4e-use-fancy-chars t
+        mu4e-headers-fields '((:human-date . 12)
+                             (:flags . 6)
+                             (:from-or-to . 22)
+                             (:subject)))
+  (setq mu4e-confirm-quit nil)
+
+  (defun my/mu4e-mark-toggle ()
+    (interactive)
+    (if (mu4e-mark-docid-marked-p mu4e~highlighted-docid)
+        (mu4e-headers-mark-and-next 'unmark)
+      (mu4e-headers-mark-and-next 'something)))
+
+  (defun my/mu4e-headers-mark-pattern ()
+    (interactive)
+    (cl-letf (((symbol-function 'mu4e~mark-get-markpair)
+               (lambda (&rest args) '(something))))
+      (mu4e-headers-mark-pattern))))
 
 (provide 'init-mu4e)

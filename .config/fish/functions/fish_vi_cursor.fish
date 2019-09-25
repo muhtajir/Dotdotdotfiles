936c4df9d1d2871385d99a0d255a431ff0346d0e
Pu Anlai <inspectormustache@users.noreply.github.com>
2019-09-25 13:15:05 +1000

    Switch to alacritty

--- .config/fish/functions/fish_vi_cursor.fish
+++ .config/fish/functions/fish_vi_cursor.fish
@@ -1,8 +1,8 @@
 function fish_vi_cursor --on-variable fish_bind_mode --on-event fish_prompt
     # only termite or this is just gonna cause countless headaches
-    if not string match -q -- '*-termite' $TERM
-        return
-    end
+    # if not string match -q -- '*-termite' $TERM
+    #     return
+    # end
 
     if [ $fish_bind_mode = "insert" ]
         echo -ne '\033[0 q'

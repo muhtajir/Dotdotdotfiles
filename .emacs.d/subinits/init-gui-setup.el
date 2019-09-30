cd1aa21bbd1decd28580fe7a571fad898e4dcb89
Pu Anlai <inspectormustache@users.noreply.github.com>
2019-10-01 00:19:46 +1000

    Load telephone-line before theme

--- .emacs.d/subinits/init-gui-setup.el
+++ .emacs.d/subinits/init-gui-setup.el
@@ -1,36 +1,3 @@
-(use-package base16-theme
-  :config
-  (defvar base16-generic-colors
-  `(:base00     ,(concat "#" (getenv "__BASE00"))
-    :base01     ,(concat "#" (getenv "__BASE01"))
-    :base02     ,(concat "#" (getenv "__BASE02"))
-    :base03     ,(concat "#" (getenv "__BASE03"))
-    :base04     ,(concat "#" (getenv "__BASE04"))
-    :base05     ,(concat "#" (getenv "__BASE05"))
-    :base06     ,(concat "#" (getenv "__BASE06"))
-    :base07     ,(concat "#" (getenv "__BASE07"))
-    :base08     ,(concat "#" (getenv "__BASE08"))
-    :base09     ,(concat "#" (getenv "__BASE09"))
-    :base0A     ,(concat "#" (getenv "__BASE0A"))
-    :base0B     ,(concat "#" (getenv "__BASE0B"))
-    :base0C     ,(concat "#" (getenv "__BASE0C"))
-    :base0D     ,(concat "#" (getenv "__BASE0D"))
-    :base0E     ,(concat "#" (getenv "__BASE0E"))
-    :base0F     ,(concat "#" (getenv "__BASE0F"))))
-
-  (deftheme base16-generic)
-  (base16-theme-define 'base16-generic base16-generic-colors)
-  ;; additional theming here:
-  (set-face-font 'default "Source Code Pro 11")
-  (setq evil-emacs-state-cursor   `(,(plist-get base16-generic-colors :base0D) box)
-        evil-insert-state-cursor  `(,(plist-get base16-generic-colors :base05) bar)
-        evil-motion-state-cursor  `(,(plist-get base16-generic-colors :base0E) box)
-        evil-normal-state-cursor  `(,(plist-get base16-generic-colors :base05) box)
-        evil-replace-state-cursor `(,(plist-get base16-generic-colors :base08) hollow)
-        evil-visual-state-cursor  `(,(plist-get base16-generic-colors :base05) box))
-  (setq pos-tip-foreground-color (plist-get base16-generic-colors :base00))
-  (setq pos-tip-background-color (plist-get base16-generic-colors :base06)))
-
 ;; GUI and Highlighting settings
 (setq inhibit-startup-message t)
 (fringe-mode '(0 . 0))
@@ -53,6 +20,12 @@
 (setq split-height-threshold 50)
 ;; WHY is vertical splitting preferred over horizontal?
 (setq split-window-preferred-function 'my/split-window-sensibly)
+
+(dolist (hook '(prog-mode-hook text-mode-hook conf-mode-hook))
+  (add-hook hook (lambda ()
+                   (setq display-line-numbers 'relative
+                         display-line-numbers-widen t
+                         display-line-numbers-current-absolute t))))
 
 ;; highlight cursor when scrolling
 (use-package beacon
@@ -80,33 +53,17 @@
 (dolist (hook '(prog-mode-hook text-mode-hook))
   (add-hook hook #'electric-pair-mode))
 
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-(use-package shackle
-  :config
-  (shackle-mode 1)
-  (setq shackle-rules
-        '(("*eshell*"
-           :regexp t :select t :popup t :align below :size 0.2)
-          ("^\\*ansi-term.*"
-           :regexp t :select t :popup t :align below :size 0.2)
-          ('inferior-python-mode
-           :select t :popup t :align below :size 0.2)
-          ('vterm-mode
-           :select t :popup t :align below :size 0.2))))
-
 ;; modeline
 (use-package telephone-line
   :config
   (telephone-line-defsegment telephone-line-my-projectile-segment ()
     (if (and (fboundp 'projectile-project-name)
              (projectile-project-p))
-      (let*
-          ((branch (car (vc-git-branches)))
-           (branch-str (unless (string= branch "master")
-                         (concat "[" branch "]"))))
-        (concat (projectile-project-name) branch-str))
+        (let*
+            ((branch (car (vc-git-branches)))
+             (branch-str (unless (string= branch "master")
+                           (concat "[" branch "]"))))
+          (concat (projectile-project-name) branch-str))
       (file-name-base (directory-file-name (file-name-directory (buffer-file-name))))))
 
   (telephone-line-defsegment telephone-line-my-flycheck-segment ()
@@ -179,10 +136,53 @@
         telephone-line-secondary-right-separator  'telephone-line-abs-right)
   (telephone-line-mode t))
 
-(dolist (hook '(prog-mode-hook text-mode-hook conf-mode-hook))
-  (add-hook hook (lambda ()
-                   (setq display-line-numbers 'relative
-                         display-line-numbers-widen t
-                         display-line-numbers-current-absolute t))))
+(use-package base16-theme
+  :config
+  (defvar base16-generic-colors
+    `(:base00     ,(concat "#" (getenv "__BASE00"))
+                  :base01     ,(concat "#" (getenv "__BASE01"))
+                  :base02     ,(concat "#" (getenv "__BASE02"))
+                  :base03     ,(concat "#" (getenv "__BASE03"))
+                  :base04     ,(concat "#" (getenv "__BASE04"))
+                  :base05     ,(concat "#" (getenv "__BASE05"))
+                  :base06     ,(concat "#" (getenv "__BASE06"))
+                  :base07     ,(concat "#" (getenv "__BASE07"))
+                  :base08     ,(concat "#" (getenv "__BASE08"))
+                  :base09     ,(concat "#" (getenv "__BASE09"))
+                  :base0A     ,(concat "#" (getenv "__BASE0A"))
+                  :base0B     ,(concat "#" (getenv "__BASE0B"))
+                  :base0C     ,(concat "#" (getenv "__BASE0C"))
+                  :base0D     ,(concat "#" (getenv "__BASE0D"))
+                  :base0E     ,(concat "#" (getenv "__BASE0E"))
+                  :base0F     ,(concat "#" (getenv "__BASE0F"))))
+
+  (deftheme base16-generic)
+  (base16-theme-define 'base16-generic base16-generic-colors)
+  ;; additional theming here:
+  (set-face-font 'default "Source Code Pro 11")
+  (setq evil-emacs-state-cursor   `(,(plist-get base16-generic-colors :base0D) box)
+        evil-insert-state-cursor  `(,(plist-get base16-generic-colors :base05) bar)
+        evil-motion-state-cursor  `(,(plist-get base16-generic-colors :base0E) box)
+        evil-normal-state-cursor  `(,(plist-get base16-generic-colors :base05) box)
+        evil-replace-state-cursor `(,(plist-get base16-generic-colors :base08) hollow)
+        evil-visual-state-cursor  `(,(plist-get base16-generic-colors :base05) box))
+  (setq pos-tip-foreground-color (plist-get base16-generic-colors :base00))
+  (setq pos-tip-background-color (plist-get base16-generic-colors :base06)))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package shackle
+  :config
+  (shackle-mode 1)
+  (setq shackle-rules
+        '(("*eshell*"
+           :regexp t :select t :popup t :align below :size 0.2)
+          ("^\\*ansi-term.*"
+           :regexp t :select t :popup t :align below :size 0.2)
+          ('inferior-python-mode
+           :select t :popup t :align below :size 0.2)
+          ('vterm-mode
+           :select t :popup t :align below :size 0.2))))
 
 (provide 'init-gui-setup)

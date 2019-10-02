e916ca05463ae1e8d4c0e0c6c5b7b507afa76593
Pu Anlai <inspectormustache@users.noreply.github.com>
2019-10-02 11:39:43 +1000

    Drop mutt in favour of mu4e

--- .config/neomutt/displayfilter.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-
-set tmpfile (mktemp)
-cat - > $tmpfile
-sed -n '1p' $tmpfile | string sub -s 7 | read dateline
-
-string replace "$dateline" (date -d $dateline) < $tmpfile
-rm $tmpfile
--- .config/neomutt/openattach.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-
-set -l tmpf (mktemp)
-cp "$argv" "$tmpf"
-
-xdg-open "$tmpf" >/dev/null &

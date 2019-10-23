14218aec8d059ad7a1aac761fca82baf7917f0dd
Pu Anlai <inspectormustache@users.noreply.github.com>
2019-10-23 22:29:06 +1100

    Use proper default sink detection in volume.pl

--- .config/i3blocks/blocklets/volume.pl
+++ .config/i3blocks/blocklets/volume.pl
@@ -1,19 +1,17 @@
 #!/usr/bin/env perl
 use strict;
-# use warnings;
+use warnings;
 # use diagnostics;
 
 $ENV{LANG} = 'C';
 
-sub get_volume_from_index {
-    # return -1 if mute
-    my $index = $_[0];
+sub get_volume {
     my $sinks_out = `pacmd list-sinks`;
 
     # clear output
-    $sinks_out =~ s/^.+?index: $index//s;
+    $sinks_out =~ s/^.+?\* index://s;
 
-    # check for mute
+    # return -1 if muted
     $sinks_out =~ /muted: (\w+)/;
     if ($1 eq "yes"){
         return -1
@@ -29,25 +27,21 @@ sub wrap_str {
     return "<span size='x-small'>" . $_[0] . "</span>\n";
 }
 
-my $sources_out = `pacmd list-sources`;
-$sources_out =~ /\* index: (\d+)/;
-my $index = $1;
-
 if ($ENV{BLOCK_BUTTON}){
     if ($ENV{BLOCK_BUTTON} == 1) {
-        system("pactl", "set-sink-mute", $index, "toggle");
+        system("pactl", "set-sink-mute", '@DEFAULT_SINK@', "toggle");
     } elsif ($ENV{BLOCK_BUTTON} == 4) {
-        system("pactl", "set-sink-volume", $index, "+5%");
+        system("pactl", "set-sink-volume", '@DEFAULT_SINK@', "+5%");
     } elsif ($ENV{BLOCK_BUTTON} == 5) {
-        system("pactl", "set-sink-volume", $index, "-5%");
+        system("pactl", "set-sink-volume", '@DEFAULT_SINK@', "-5%");
     } elsif ($ENV{BLOCK_BUTTON} == 6) {
-        system("pactl", "set-sink-volume", $index, "+1%");
+        system("pactl", "set-sink-volume", '@DEFAULT_SINK@', "+1%");
     } elsif ($ENV{BLOCK_BUTTON} == 7) {
-        system("pactl", "set-sink-volume", $index, "-1%");
+        system("pactl", "set-sink-volume", '@DEFAULT_SINK@', "-1%");
     }
 }
 
-my $volume = get_volume_from_index($index);
+my $volume = get_volume();
 
 if ($volume < 0) {
     print(wrap_str(""));
